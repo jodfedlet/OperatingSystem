9baed763963f97078019f8f723a17b38dab8d78a
Your Name <you@example.com>
2019-10-03 12:57:09 -0300

    Parte Inicial da implementação de Lottery Scheduling

---
+++ loteria.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int number_process;
+
+    printf("Entrar a quantidade de processo: ");
+    scanf("%d", &number_process);
+
+    if(number_process > 0 && number_process < 3){
+
+            //Implementation
+    }
+    else{
+        printf("A quantidade de processo nao pode ultrapassar 3 nem pode ser menor do que 0");
+    }
+
+}
