c1a9fe1ca6325e58246efbda944a83a7a9f5b5a8
Your Name <you@example.com>
2019-10-03 13:12:19 -0300

    Lendo Processos

--- loteria.c
+++ loteria.c
@@ -3,16 +3,20 @@
 
 int main(){
     int number_process;
+    char process[5];
 
     printf("Entrar a quantidade de processo: ");
     scanf("%d", &number_process);
 
     if(number_process > 0 && number_process <= 3){
-
-            //Implementation
+        //lendo os processo
+        for(int i = 0; i< number_process; i++){
+            printf("Digite o processo [%d]: ",i+1);
+            scanf("%s",process);
+        }
     }
     else{
-        printf("A quantidade de processo nao pode ultrapassar 3 nem pode ser menor do que 0");
+        printf("A quantidade de processo nao pode ultrapassar 3 nem pode ser menor do que 0\n");
     }
 
 }
