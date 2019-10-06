1b49739d4fa9e0148ad284402c8d7cf5f6ee9d91
Your Name <you@example.com>
2019-10-06 11:57:30 -0300

    Adicionar caracteríscas do processo

--- loteria/proc.h
+++ loteria/proc.h
@@ -52,6 +52,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int bilhetes;                // Quantidade de bilhetes do processo
+  int QuantVezSelecionado;     //Quantidade de vezes que o processo foi selecionado pelo Escalonador
 };
 
 // Process memory is laid out contiguously, low addresses first:
